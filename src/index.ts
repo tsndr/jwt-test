f2bd443ce4d84650f820652133fca08b048a29d5
Toby <mail@tobias-schneider.io>
2023-11-16 13:19:49 +0100

    add @ sign

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const secret = 'super-secret'
 
-		const token = await jwt.sign({ sub: 'test' }, secret)
+		const token = await jwt.sign({ sub: 'test', more: '@test' }, secret)
 		console.log({ token })
 
 		const verified = await jwt.verify(token, secret)
