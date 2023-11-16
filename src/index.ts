0264f09356454e39b88f94f464d17eb533620df0
Toby <mail@tobias-schneider.io>
2023-11-16 13:07:33 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import jwt from '@tsndr/cloudflare-worker-jwt'
+
+export type Env = {}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const secret = 'super-secret'
+
+		const token = await jwt.sign({ sub: 'test' }, secret)
+		console.log({ token })
+
+		const verified = await jwt.verify(token, secret)
+		console.log({ verified })
+
+		return Response.json({ token, verified })
+	}
+}
